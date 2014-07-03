ab11593d5a0d8cee92e2a2b5cb3140d4c634f310
Robert Fauver <robertfauver@gmail.com>
2014-07-03 12:18:04 -0400

    adding the yeoman ng install, angular-ui via bower, basic braindump kinda stuff in the README

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Browser based Chat Rooms based upon different MVCs. Inspired by todoMVC
 
 ##TODO
 [ ] AngularJS based Chat App
+- [ ] Flesh out the UI, as a wireframe sketch, show photo in /about/
+- [ ] R&D other online chats
+
+###BRAINDUMP
+- input for text
+- field to show
+- gravatar?
+- presence online or not with shapes
+- nice to have group chat
+- button to open/close
---
+++ app/scripts/controllers/chatController.js
@@ -0,0 +1,17 @@
+/**
+ * @ngdoc function
+ * @name ngChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngChatApp
+ */
+angular.module('ngChatApp')
+    .controller('ChatCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
+
+'use strict';
