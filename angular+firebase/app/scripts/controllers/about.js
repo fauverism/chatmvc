2cd26b5c231ff3f3763fb456719310bdffdb7e76
fauverism <robertfauver@gmail.com>
2014-07-04 11:53:39 -0400

    inital add of content for the ngFireChat

---
+++ angular+firebase/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngFireChatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngFireChatApp
+ */
+angular.module('ngFireChatApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angular+firebase/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngFireChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngFireChatApp
+ */
+angular.module('ngFireChatApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
