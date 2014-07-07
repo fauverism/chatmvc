5b2ba8b71dce32140938821af755bc87cb5c9ec3
fauverism <robertfauver@gmail.com>
2014-07-07 14:09:03 -0400

    adding PubNub

---
+++ angular+PubNub/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngPubNubChatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngPubNubChatApp
+ */
+angular.module('ngPubNubChatApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angular+PubNub/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngPubNubChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngPubNubChatApp
+ */
+angular.module('ngPubNubChatApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
